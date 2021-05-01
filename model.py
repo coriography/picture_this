04782fd9cfe1a3fcf970e76f67138936d82a85a6
Cori Lint <corilint@gmail.com>
2021-05-01 18:18:09 -0500

    created User db model

--- model.py
+++ model.py
@@ -2,7 +2,39 @@ from flask_sqlalchemy import SQLAlchemy
 
 import bcrypt
 
+import os
+
 db = SQLAlchemy()
+
+
+# os.system('dropdb pt')
+# os.system('createdb pt')
+
+# db.create_all()
+
+# db.session.add(n)
+# db.session.commit()
+
+
+class User(db.Model):
+    """Data model for a user."""
+
+    __tablename__ = "users"
+
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    username = db.Column(db.String(15), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    # password_hashed = db.Column(db.Binary(128), nullable=False)
+
+    def check_password(self, password):
+        encoded_password = password.encode("utf-8")
+        return bcrypt.checkpw(encoded_password, self.password_hashed)
+
+    def __repr__(self):
+        """Display info about User."""
+
+        return f'<User user_id={self.user_id}, username={self.username}, email={self.email}>'
+
 
 def connect_to_db(flask_app, db_uri='postgresql:///pt', echo=True):
     flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
@@ -12,4 +44,11 @@ def connect_to_db(flask_app, db_uri='postgresql:///pt', echo=True):
     db.app = flask_app
     db.init_app(flask_app)
 
-    print('Connected to the db!')
+    print('Connected to the db!')    
+
+
+if __name__ == '__main__':
+    print("we're in model")
+    from server import app
+
+    connect_to_db(app)
