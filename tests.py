810daec8d6f9b98d251a31b2440fa1c4df77b0b2
Cori Lint <corilint@gmail.com>
2021-05-01 15:41:08 -0500

    initial commit of testing

---
+++ tests.py
@@ -0,0 +1,21 @@
+import server
+import unittest
+
+class serverTests(unittest.TestCase):
+    """Tests for Picture This app."""
+
+    def setUp(self):
+        """Code to run before every test."""
+
+        self.client = server.app.test_client() # test_client from Werkzeug library returns a "browser" to "run" app
+        server.app.config['TESTING'] = True
+
+    def test_homepage(self):
+        """Does homepage load?"""
+
+        result = self.client.get("/")
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b"Welcome", result.data)
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
