eff160817e19c4c7dd0d80e40635af4f2fa9dc86
Cori Lint <corilint@gmail.com>
2021-05-01 15:48:52 -0500

    added db model and related requirements

---
+++ model.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_to_db(flask_app, db_uri='postgresql:///pt', echo=True):
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    # flask_app.config['SQLALCHEMY_ECHO'] = echo
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print('Connected to the db!')
--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 import jinja2
 import os
+from model import connect_to_db
 
-app = Flask(__name__)
-app.secret_key = os.environ["SECRET_KEY"]
-app.jinja_env.undefined = jinja2.StrictUndefined
+app = Flask(__name__) 
+app.secret_key = os.environ["SECRET_KEY"] # use key exported from secrets.sh
+app.jinja_env.undefined = jinja2.StrictUndefined # throw an error for an undefined Jinja var
 
 @app.route("/")
 def index():
@@ -13,4 +14,5 @@ def index():
     return render_template("home.html")
 
 if __name__ == "__main__":
+    connect_to_db(app)
     app.run(debug=True, host="0.0.0.0")
