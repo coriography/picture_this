0e8ab4ebf15855a8b5d983342905dd217c41bb60
Cori Lint <corilint@gmail.com>
2021-05-05 17:30:42 -0500

    initial commit

---
+++ seed_data.py
@@ -0,0 +1,17 @@
+data = {
+    1: {
+        'name': 'Guppy',
+        'email': 'guppy@thecat.com',
+        'password': 'badpw'
+    },
+    2: {
+        'name': 'Loja',
+        'email': 'loja@thecat.com',
+        'password': 'vbadpw'
+    }
+    3: {
+        'name': 'Cori',
+        'email': 'cori@picturethis.com',
+        'password': 'pwwwww'
+    }
+}
