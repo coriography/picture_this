bc2722a16ea9502d8fd06386abb92a0c210c87db
Cori Lint <corilint@gmail.com>
2021-05-05 18:26:40 -0500

    added images to seed data

--- seed_data.py
+++ seed_data.py
@@ -2,13 +2,29 @@ data = {
     1: {
         'name': 'Guppy',
         'email': 'guppy@thecat.com',
-        'password': 'badpw'
+        'password': 'badpw',
+        'images': [
+            {
+                'url': 'https://s.keepmeme.com/files/en_posts/20200822/cc83fa3c7f8f8d04b3cdb12d65d57101confused-cat-with-a-lot-of-question-marks.jpg',
+                'notes': 'this cat is CONFUSE',
+                'private': False,
+                'user': 1,
+                'tag': None
+            },
+            {
+                'url': 'https://s.keepmeme.com/files/en_posts/20200822/cc83fa3c7f8f8d04b3cdb12d65d57101confused-cat-with-a-lot-of-question-marks.jpg',
+                'notes': '222 this cat is CONFUSE',
+                'private': True,
+                'user': 1,
+                'tag': None
+            }
+        ]
     },
     2: {
         'name': 'Loja',
         'email': 'loja@thecat.com',
         'password': 'vbadpw'
-    }
+    },
     3: {
         'name': 'Cori',
         'email': 'cori@picturethis.com',
