fa476f09755e012499db17dc0fba90ecfe821261
Cori Lint <corilint@gmail.com>
2021-05-05 17:31:50 -0500

    added dummy db users

--- seed.py
+++ seed.py
@@ -1,12 +1,19 @@
 import server
 import model
 import os
+from helpers import *
+from seed_data import data
 
 os.system('dropdb pt')
 os.system('createdb pt')
 
 model.connect_to_db(server.app)
 model.db.create_all()
+
+##### * create users * #####
+
+for i in data:
+    register_user(data[i]['name'], data[i]['email'], data[i]['password'])
 
 if __name__ == '__main__':
     print("we're in seed")
