8f9840470d0e9cb6518321e67d175c4c9af8356c
Cori Lint <corilint@gmail.com>
2021-05-06 18:38:08 -0500

    added register form, post route, and message container

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect
 import jinja2
 import os
 from model import connect_to_db
@@ -6,6 +6,9 @@ from model import connect_to_db
 app = Flask(__name__) 
 app.secret_key = os.environ["SECRET_KEY"] # use key exported from secrets.sh
 app.jinja_env.undefined = jinja2.StrictUndefined # throw an error for an undefined Jinja var
+
+
+##### * RENDER PAGES * #####
 
 @app.route("/")
 def go_home():
@@ -24,6 +27,30 @@ def show_upload_page():
     """Return My Board page."""
 
     return render_template("upload.html")
+
+
+##### * API * #####
+
+@app.route('/api/register_user', methods=['POST'])
+def register_new_user():
+    """Add a new user to the database."""
+    
+    username = request.form['username']
+    email = request.form['email']
+    password = request.form['password']
+
+    flash(f'Account created for {username}, {email}')
+    return redirect('/')
+
+    # if username and email are not already in db,
+    #     session['user'] = username
+    #     flash(f'Logged in as {username}')
+    #     return redirect('/')
+
+    # else:
+    #     flash('Wrong password!')
+    #     return redirect('/login')
+    
 
 if __name__ == "__main__":
     connect_to_db(app)
