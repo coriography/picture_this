98c3b6206f064a4d8f99ef9a30deb1d76675f7e1
Cori Lint <corilint@gmail.com>
2021-05-07 14:18:19 -0500

    added registration helper functions and organized file

--- helpers.py
+++ helpers.py
@@ -1,6 +1,24 @@
 from model import db, User, Image, Tag
 
+
+##### * General queries * #####
+
+def get_all_users():
+    return User.query.all()
+
+
+##### * User registration & login * #####
+
+def check_username(username):
+    """Check whether given username exists in users table."""
+    return User.query.filter(User.username == username).first()
+
+def check_email(email):
+    """Check whether given email exists in users table."""
+    return User.query.filter(User.email == email).first()
+
 def register_user(username, email, password):
+    """Add a user to the database."""
     user = User(
         username=username, 
         email=email, 
@@ -9,6 +27,13 @@ def register_user(username, email, password):
     db.session.commit()
 
     return user
+
+# get user by email
+# check whether password matches (existing function)
+# login user - add user to session
+
+
+##### * Image upload * #####
 
 def upload_image(url, notes, user_id, private=False, tag_id=None):
     image = Image(
@@ -22,3 +47,4 @@ def upload_image(url, notes, user_id, private=False, tag_id=None):
     db.session.commit()
 
     return image
+    
